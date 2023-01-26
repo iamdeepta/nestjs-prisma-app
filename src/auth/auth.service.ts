2e69e888c487db29dcf8b64fa7207649e3fd1693
iamdeepta <deepta.barua@northsouth.edu>
2023-01-26 10:30:21 +0600

    updated user related apis

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { User } from "@prisma/client";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable({})
 export class AuthService{
     constructor(private prisma: PrismaService){}
 
-    signup(){
-        return {msg: "Hello Signup"}
+    async signup(dto: AuthDto){
+
+        try{
+
+            const user = await this.prisma.user.create({
+                data:{
+                    name: dto.name,
+                    email: dto.email,
+                    password: dto.password
+                }
+            });
+
+            delete user.password;
+
+            return user;
+
+        }catch(error){
+            return error;
+        }
+        
     }
 
 
