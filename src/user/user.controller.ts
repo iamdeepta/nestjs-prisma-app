969d02604ac024ca9fb208c76b0256b481373053
iamdeepta <deepta.barua@northsouth.edu>
2023-02-09 18:31:59 +0600

    Implement auth mechanism

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
-    @Get("getUsers")
-    getUsers(){
-        return this.userService.getUsers();
-    }
+  @UseGuards(JwtGuard)
+  @Get('getUsers')
+  getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    console.log(user);
+    return user;
+  }
 }
